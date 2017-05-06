907febb4c88d957beab3b16cdfe99079dc284c97
grecian_urn <ljzhu_fdu@163.com>
2017-05-06 14:08:38 +0800

    Airplane test

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -1,4 +1,5 @@
 #include "MainScene.h"
+#include <random>
 
 USING_NS_CC;
 
@@ -28,19 +29,40 @@ bool MainScene::init()
 	label->setPosition(Vec2(origin.x + visibleSize.width / 2,
 		origin.y + visibleSize.height - label->getContentSize().height));
 	this->addChild(label,0);
-	airplane = Sprite::create("Picture/airplane.png");
-//	 auto airplane = Sprite::create("Picture/airplane.png");
 
-	airplane->setScale(0.1, 0.1);
-	airplane->setPosition(200, 200);
-	airplane->setTag(12);
-	this->addChild(airplane,1);
+	std::random_device rd;						//采用非确定性随机数发生器产生随机数种子
+	std::default_random_engine gen(rd());		//采用默认随机数引擎产生随机数
+	std::uniform_int_distribution<> unif_x(origin.x, origin.x + visibleSize.width);		//采用整数均匀分布器产生x均匀分布的随机数
+	std::uniform_int_distribution<> unif_y(origin.y, origin.y + visibleSize.height);		//采用整数均匀分布器产生y均匀分布的随机数
+
+	for (int i = 0; i < 5; i++)
+	{
+		Airplane* plane;
+		plane = Airplane::createPlane("Picture/airplane.png");
+		plane->setScale(0.1, 0.1);
+		plane->setPosition(unif_x(gen), unif_y(gen));
+		this->addChild(plane);
+		this->my_planes.push_back(plane);
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		Airplane* plane;
+		plane = Airplane::createPlane("Picture/airplane_red.png");
+		plane->setScale(0.1, 0.1);
+		plane->setPosition(unif_x(gen), unif_y(gen));
+		this->addChild(plane);
+		this->enemy_planes.push_back(plane);
+	}
+	
+
 	this->schedule(schedule_selector(MainScene::update));
 
 
 	auto listen = EventListenerTouchOneByOne::create();
 	listen->onTouchBegan = CC_CALLBACK_2(MainScene::onTouchBegan, this);
 	listen->onTouchMoved = CC_CALLBACK_2(MainScene::onTouchMoved, this);
+	listen->onTouchEnded = CC_CALLBACK_2(MainScene::onTouchEnded, this);
 	listen->setSwallowTouches(true);
 	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listen, this);
 
@@ -50,39 +72,98 @@ bool MainScene::init()
 
 void MainScene::update(float f)
 {
-//	
-//	auto sp = this->getChildren();//获取这个层中所有的孩子,也就是所有的精灵，看不懂？别逗我了，点进去看源码吧，注意它的返回值类型。  
-//	for (auto a : sp)
-//	{
-//		if(a->getBoundingBox().containsPoint(touchPoint))
-//			continue;
-//		auto esp = Point(touchPoint.x - a->getPosition().x, touchPoint.y - a->getPosition().y).getNormalized();
-//		a->setPosition(a->getPosition()+esp*moveSpeed);
-//		
-//	}
-	if (airplane->getBoundingBox().containsPoint(touchPoint))
-		return;
-	auto esp = (touchPoint - airplane->getPosition()).getNormalized();
-	airplane->setPosition(airplane->getPosition()+esp*moveSpeed);
-
+	for (auto my_plane : this->my_planes)
+		if (my_plane->isActive())
+			my_plane->update();
 
 	log("touch point:%f,%f",touchPoint.x,touchPoint.y);
 }
 
 bool MainScene::onTouchBegan(cocos2d::Touch* pTouch, cocos2d::Event*)
 {
-	this->touchPoint = pTouch->getLocation();
+	
+	Point touch = pTouch->getLocation();//返回点击的位置
+	this->touchPoint = touch;
+
+	if (state == 0)
+	{
+		for (auto my_plane : this->my_planes)
+			if (my_plane->getBoundingBox().containsPoint(touch))
+			{
+				my_plane->select();
+				this->state = 1;
+				return true;
+			}
+	}
+
+	if (this->state == 1)
+	{
+		for (auto my_plane : my_planes)
+			if (my_plane->isSelected())
+			{
+				my_plane->setDest(touch);
+				my_plane->activate();
+			}
+		this->state = 0;
+	}
+	else
+	{
+		this->state = 2;
+		this->mouse_rect = DrawNode::create();
+		this->addChild(this->mouse_rect, 2);
+	}
+
 	return true;
 }
 
 void MainScene::onTouchMoved(cocos2d::Touch* pTouch, cocos2d::Event* pEvent)
 {
-	auto airplane = getChildByTag(12);
 	Point touch = pTouch->getLocation();//返回点击的位置  
-	Rect rectPlayer = airplane->getBoundingBox();//看返回值类型，应该知道这个是飞机所占矩形区域的大小  
 
-	if (rectPlayer.containsPoint(touch)) {//如果点击的点在这个矩形区域内就可以对飞机进行拖动  
-		Point temp = pTouch->getDelta();
-		airplane->setPosition(airplane->getPosition() + temp);
+	//
+	if (this->state == 2)
+	{
+		this->mouse_rect->clear();
+		Vec2 mouse_rect_points[4];
+		mouse_rect_points[0] = this->touchPoint;
+		mouse_rect_points[1] = Vec2(this->touchPoint.x, touch.y);
+		mouse_rect_points[2] = touch;
+		mouse_rect_points[3] = Vec2(touch.x, this->touchPoint.y);
+
+		//绘制空心多边形
+		//填充颜色：Color4F(1, 0, 0, 0), 透明
+		//轮廓颜色：Color4F(0, 1, 0, 1), 绿色
+		this->mouse_rect->drawPolygon(mouse_rect_points, 4, Color4F(1, 0, 0, 0), 1, Color4F(0, 1, 0, 1));
+		//
 	}
+}
+
+void MainScene::onTouchEnded(cocos2d::Touch* pTouch, cocos2d::Event* pEvent)
+{
+	if (this->state == 2)
+	{
+		this->removeChild(this->mouse_rect);
+		this->state = 1;
+
+		Point touch = pTouch->getLocation();//返回点击的位置  
+
+		Rect select_rect{ this->touchPoint.x, this->touchPoint.y, touch.x - this->touchPoint.x, this->touchPoint.y - touch.y};
+
+		for (auto my_plane : my_planes)
+			if (select_rect.containsPoint(my_plane->getPosition()))
+				my_plane->select();
+	}
+}
+
+void Airplane::update()
+{
+	if (selected)
+		if ((dest - getPosition()).getLength() < 10)
+			active = 0;
+		else
+		{
+			active = 1;
+			auto esp = (dest - getPosition()).getNormalized();
+			setPosition(getPosition() + esp * move_speed);
+		}
 }
--- Classes/MainScene.h
+++ Classes/MainScene.h
@@ -3,6 +3,37 @@
 
 #include "cocos2d.h"
 
+class Airplane : public cocos2d::Sprite
+{
+public:
+	void update();
+	bool isActive() { return active; }
+	bool isSelected() { return selected; }
+	void select() { selected = true; }
+	void unselect() { selected = false; }
+	void activate() { active = true; }
+	void setDest(cocos2d::Point destination) { this->dest = destination; }
+	static Airplane* createPlane(const std::string& filename)
+	{
+		Airplane *airplane = new (std::nothrow) Airplane();
+		if (airplane && airplane->initWithFile(filename))
+		{
+			airplane->autorelease();
+			return airplane;
+		}
+		CC_SAFE_DELETE(airplane);
+		return nullptr;
+	}
+private:
+	bool selected = 0;
+	bool active = 0;
+	int state = 0;
+
+	const float move_speed = 10.0;
+
+	cocos2d::Point dest;
+};
+
 class MainScene : public cocos2d::Layer
 {
 public:
@@ -11,12 +42,21 @@ public:
 	void update(float f) override;
 	bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*)override;
 	void onTouchMoved(cocos2d::Touch*, cocos2d::Event*)override;
+	void onTouchEnded(cocos2d::Touch*, cocos2d::Event*)override;
 
 	CREATE_FUNC(MainScene);
 private:
-	cocos2d::Sprite* airplane;
+	std::vector<Airplane*> my_planes;
+	std::vector<Airplane*> enemy_planes;
+	cocos2d::DrawNode* mouse_rect;
+	int state = 0;
 	cocos2d::Vec2 touchPoint{500,500};
-	float moveSpeed = 10.0;
+	
+
+	//EventListenerMouse* _mouseListener;
 };
 
+
+
 #endif
+
